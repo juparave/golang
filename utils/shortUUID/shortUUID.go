5bed8f84d4ac877acf24c2b7a028750a04a1368f
Juan Pablo TankerMini <juparave@deuxbits.com>
2021-12-07 10:33:52 -0600

    Adding SortableShortUUID, a timestamp based generated id

--- utils/shortUUID/shortUUID.go
+++ utils/shortUUID/shortUUID.go
@@ -2,26 +2,38 @@ package shortuuid
 
 import (
 	"math/rand"
+	"time"
 )
 
-const alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_"
+const numerals = "0123456789ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+const alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz"
 
 func ShortUUID(length int) string {
+	// seed a new source of randon numbers
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
 	// get int value of uuid
-	uuid := rand.Int63()
+	uuid := r.Int63()
 	numerals := alphabet
 
-	// encode it to base62 until we get the length we want
+	// encode it until we get the length we want
 	shortuuid := encode(uuid, len(numerals), numerals)
 	for len(shortuuid) < length {
-		uuid = rand.Int63()
+		uuid = r.Int63()
 		shortuuid = encode(uuid, len(numerals), numerals)
 		// if length is too long, append a second encoded uuid
-		if length > 11 { // 12 is the max length of base62
-			shortuuid += encode(rand.Int63(), len(numerals), numerals)
+		if length > 11 { // 11 is the max length using alphabet, probably =)
+			shortuuid += encode(r.Int63(), len(numerals), numerals)
 		}
 	}
 	return shortuuid[0:length]
+}
+
+func SortableShortUUID() string {
+	// use time.Now() to get a unix timestamp
+	// the length of the timestamp is 11 using all numerals
+	// this UUID is sortable and can be used as a key in a map
+	return encode(time.Now().UnixNano(), len(alphabet), alphabet)
 }
 
 func encode(num int64, base int, numerals string) string {
--- utils/shortUUID/shortUUID_test.go
+++ utils/shortUUID/shortUUID_test.go
@@ -2,37 +2,33 @@ package shortuuid
 
 import "testing"
 
+type Tests struct {
+	input    int // wanted length
+	expected int // expected length
+}
+
 func TestShortUUID(t *testing.T) {
-	uuid := ShortUUID(6)
-	if len(uuid) != 6 {
-		t.Error("UUID length is not 6")
+
+	tests := []Tests{
+		{3, 3},
+		{4, 4},
+		{5, 5},
+		{6, 6},
+		{7, 7},
+		{8, 8},
+		{9, 9},
+		{10, 10},
+		{11, 11},
+		{12, 12},
+		{13, 13},
+		{14, 14},
 	}
-	uuid = ShortUUID(7)
-	if len(uuid) != 7 {
-		t.Error("UUID length is not 7")
+
+	for _, test := range tests {
+		actual := ShortUUID(test.input)
+		if len(actual) != test.expected {
+			t.Errorf("ShortUUID(%d) = %d, expected %d", test.input, len(actual), test.expected)
+		}
 	}
-	uuid = ShortUUID(8)
-	if len(uuid) != 8 {
-		t.Error("UUID length is not 8")
-	}
-	uuid = ShortUUID(9)
-	if len(uuid) != 9 {
-		t.Error("UUID length is not 9")
-	}
-	uuid = ShortUUID(10)
-	if len(uuid) != 10 {
-		t.Error("UUID length is not 10")
-	}
-	uuid = ShortUUID(11)
-	if len(uuid) != 11 {
-		t.Error("UUID length is not 11")
-	}
-	uuid = ShortUUID(12)
-	if len(uuid) != 12 {
-		t.Error("UUID length is not 12")
-	}
-	uuid = ShortUUID(13)
-	if len(uuid) != 13 {
-		t.Error("UUID length is not 13")
-	}
+
 }
